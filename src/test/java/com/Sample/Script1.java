c36632f27268295fa239ea554ea1d4fd2faf1035
Dibendu95 <107481501+Dibendu95@users.noreply.github.com>
2022-08-24 16:51:35 +0530

    done

--- src/test/java/com/Sample/Script1.java
+++ src/test/java/com/Sample/Script1.java
@@ -7,7 +7,7 @@ System.out.println("GitHub Sample");
 	System.out.println("update");
 
 	System.out.println("abc");
-	
+	System.out.println("");
 
 	System.out.println("hello");
 	System.out.println("abc");
