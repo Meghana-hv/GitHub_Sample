94dd25693ac6540179da90f709e4d2a88dd133dd
info <info@DESKTOP-11MTSOP>
2022-08-24 16:12:33 +0530

    new

---
+++ src/test/java/com/Sample/DemoClass1.java
@@ -0,0 +1,10 @@
+package com.Sample;
+
+public class DemoClass1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello.............");
+		System.out.println("Hi");
+		System.out.println("bye");
+	}
+}
---
+++ src/test/java/com/Sample/Script1.java
@@ -0,0 +1,11 @@
+package com.Sample;
+
+public class Script1 {
+
+	public static void main(String[] args) {
+System.out.println("GitHub Sample");
+	System.out.println("update");
+	System.out.println("abc");
+	}
+
+}
