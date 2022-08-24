337e39b60cb6fbc16c79cc4ff64c979f0a186c4c
Dibendu95 <107481501+Dibendu95@users.noreply.github.com>
2022-08-24 16:49:59 +0530

    done

---
+++ src/test/java/com/Sample/Gitproject.java
@@ -0,0 +1,10 @@
+package com.Sample;
+
+public class Gitproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println(" test lead ");
+	}
+
+}
--- src/test/java/com/Sample/Script1.java
+++ src/test/java/com/Sample/Script1.java
@@ -5,13 +5,13 @@ public class Script1 {
 	public static void main(String[] args) {
 System.out.println("GitHub Sample");
 	System.out.println("update");
-<<<<<<< HEAD
-	System.out.println("abc");
-	}
-=======
-	System.out.println("hello");}
+
 	System.out.println("abc");
 	
->>>>>>> branch 'master' of https://github.com/Meghana-hv/GitHub_Sample.git
 
+	System.out.println("hello");
+	System.out.println("abc");
+	
+
+}
 }
