088a4e5ec7045061016d75dbb35e056a76787b81
info <info@DESKTOP-11MTSOP>
2022-08-24 16:02:33 +0530

    check

--- src/test/java/com/Sample/Script1.java
+++ src/test/java/com/Sample/Script1.java
@@ -4,6 +4,8 @@ public class Script1 {
 
 	public static void main(String[] args) {
 System.out.println("GitHub Sample");
-	System.out.println("update");}
+	System.out.println("update");
+	System.out.println("abc");
+	}
 
 }
